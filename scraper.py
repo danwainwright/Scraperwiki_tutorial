15bf0a6350d4f69ce41a56e8a0723a049a8f8a90
danwainwright <daniel.wainwright@bbc.co.uk>
2017-06-14 20:18:47 +0100

    Create scraper.py

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ html = scraperwiki.scrape('http://inmo.ie/6022')
 import lxml.html
 root = lxml.html.fromstring(html) # turn our HTML into an lxml object
 td = root.cssselect('td') # get all the <tr> tags
-for tr in trs:
+for td in tds:
      record = td.text_content() # just the text inside the HTML tag
 for td in tds:
      record = { "td" : td.text } # column name and value
