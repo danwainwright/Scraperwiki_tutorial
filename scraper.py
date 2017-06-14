92ed85d4423e586f5b9d22d406afdee3a189a1aa
danwainwright <daniel.wainwright@bbc.co.uk>
2017-06-14 20:07:43 +0100

    Create scraper.py

--- scraper.py
+++ scraper.py
@@ -2,9 +2,9 @@ import scraperwiki
 html = scraperwiki.scrape('http://inmo.ie/6022')
 import lxml.html
 root = lxml.html.fromstring(html) # turn our HTML into an lxml object
-tds = root.cssselect('td') # get all the <td> tags
-for td in tds:
-     print td.text_content() # just the text inside the HTML tag
-for td in tds:
-     record = { "td" : td.text_content } # column name and value
-     scraperwiki.sqlite.save(["td"], record) # save the records one by one
+tdr = root.cssselect('tr') # get all the <tr> tags
+for tr in trs:
+     print tr.text_content() # just the text inside the HTML tag
+for tr in trs:
+     record = { "tr" : tr.text_content } # column name and value
+     scraperwiki.sqlite.save(["tr"], record) # save the records one by one
