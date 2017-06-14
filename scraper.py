e775693228b43856a81c78b0c416890c66d0266e
danwainwright <daniel.wainwright@bbc.co.uk>
2017-06-14 20:09:47 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,7 +2,7 @@ import scraperwiki
 html = scraperwiki.scrape('http://inmo.ie/6022')
 import lxml.html
 root = lxml.html.fromstring(html) # turn our HTML into an lxml object
-tdr = root.cssselect('tr') # get all the <tr> tags
+tr = root.cssselect('tr') # get all the <tr> tags
 for tr in trs:
      print tr.text_content() # just the text inside the HTML tag
 for tr in trs:
